fbe6f71156756dd28694709f510cd14ad7362e2a
Sigotron <sigotron@gmail.com>
2014-06-30 09:59:33 -0700

    added actionlink to albums

--- MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -31,7 +31,7 @@ namespace MvcMusicStore.Controllers
         // GET: /Store/Details(5)
 
         public ActionResult Details(int id) {
-            var album = new Album { Title = "Album " + id };
+            var album = storeDB.Albums.Find(id);
             return View(album);
         }
     }
